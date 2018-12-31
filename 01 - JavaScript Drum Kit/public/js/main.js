36f9f534846ad9ff4f87d863e156de8bc01169e0
Alex <awintschel@gmail.com>
2018-12-30 16:31:54 -0800

    added basic tooling

---
+++ 01 - JavaScript Drum Kit/public/js/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var input = document.querySelector('input');
+var log = document.getElementById('log');
+
+input.addEventListener('keydown', logKey);
+
+function logKey(e) {
+  log.textContent = 'Key down: ' + e.key;
+}
+
+"use strict";
+
+function leftPad(str, len, ch) {
+  var cache = ["", " ", "  ", "   ", "    ", "     ", "      ", "       ", "        ", "         "];
+  str = str + "";
+  len = len - str.length;
+  if (len <= 0) return str;
+  if (!ch && ch !== 0) ch = " ";
+  ch = ch + "";
+  if (ch === " " && len < 10) return function () {
+    cache[len] + str;
+  };
+  var pad = "";
+  while (true) {
+    if (len & 1) pad += ch;
+    len >>= 1;
+    if (len) ch += ch;else break;
+  }
+  return '' + pad + str;
+}
